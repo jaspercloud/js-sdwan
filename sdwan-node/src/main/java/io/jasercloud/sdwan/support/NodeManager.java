526fff30ba665d3bf5ef1e7ddc86d61f02109d2c
jaspercloud <qq276726581@gmail.com>
2023-10-08 16:48:35 +0800

    init

--- sdwan-node/src/main/java/io/jasercloud/sdwan/support/NodeManager.java
+++ sdwan-node/src/main/java/io/jasercloud/sdwan/support/NodeManager.java
@@ -4,21 +4,53 @@ import io.jasercloud.sdwan.*;
 import io.jaspercloud.sdwan.AsyncTask;
 import io.jaspercloud.sdwan.core.proto.SDWanProtos;
 import io.jaspercloud.sdwan.exception.ProcessException;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 
 import java.net.InetSocketAddress;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class NodeManager {
+@Slf4j
+public class NodeManager implements InitializingBean {
 
     private SDWanNode sdWanNode;
     private StunClient stunClient;
+
+    private Map<String, NodeHeart> nodeHeartMap = new ConcurrentHashMap<>();
 
     public NodeManager(SDWanNode sdWanNode, StunClient stunClient) {
         this.sdWanNode = sdWanNode;
         this.stunClient = stunClient;
     }
 
-    public InetSocketAddress getPublicAddress(SDWanProtos.SDArpResp sdArpResp) {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                for (Map.Entry<String, NodeHeart> entry : nodeHeartMap.entrySet()) {
+                    NodeHeart nodeHeart = entry.getValue();
+                    long diffTime = System.currentTimeMillis() - nodeHeart.getLastHeart();
+                    if (diffTime > (30 * 1000)) {
+                        stunClient.sendBind(nodeHeart.getAddress())
+                                .thenAccept(packet -> {
+                                    nodeHeart.setLastHeart(System.currentTimeMillis());
+                                });
+                    }
+                }
+                try {
+                    Thread.sleep(10 * 1000);
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
+        }, "node-heart");
+        thread.start();
+    }
+
+    public CompletableFuture<InetSocketAddress> getPublicAddress(SDWanProtos.SDArpResp sdArpResp) {
         try {
             String vip = sdArpResp.getVip();
             String stunMapping = sdArpResp.getStunMapping();
@@ -28,17 +60,22 @@ public class NodeManager {
             if (StunRule.EndpointIndependent.equals(self.getFiltering())
                     && StunRule.EndpointIndependent.equals(stunFiltering)) {
                 InetSocketAddress resp = new InetSocketAddress(sdArpResp.getPublicIP(), sdArpResp.getPublicPort());
-                return resp;
+                return CompletableFuture.completedFuture(resp);
             } else if (StunRule.EndpointIndependent.equals(self.getFiltering())) {
                 String tranId = StunMessage.genTranId();
-                CompletableFuture<StunPacket> waitTask = AsyncTask.waitTask(tranId, 3000);
+                CompletableFuture<StunPacket> future = AsyncTask.waitTask(tranId, 3000);
                 sdWanNode.punching(address.getHostString(), address.getPort(), vip, tranId);
-                InetSocketAddress resp = waitTask.get().recipient();
-                return resp;
+                return future.thenApply(e -> e.recipient()).thenApply(addr -> {
+                    nodeHeartMap.put(vip, new NodeHeart(addr, System.currentTimeMillis()));
+                    return addr;
+                });
             } else if (StunRule.EndpointIndependent.equals(stunFiltering)) {
                 InetSocketAddress target = new InetSocketAddress(sdArpResp.getPublicIP(), sdArpResp.getPublicPort());
-                StunPacket stunPacket = stunClient.sendBind(target);
-                return stunPacket.recipient();
+                CompletableFuture<StunPacket> future = stunClient.sendBind(target);
+                return future.thenApply(e -> e.recipient()).thenApply(addr -> {
+                    nodeHeartMap.put(vip, new NodeHeart(addr, System.currentTimeMillis()));
+                    return addr;
+                });
             } else if (StunRule.AddressDependent.equals(self.getFiltering())) {
 
             } else if (StunRule.AddressDependent.equals(stunFiltering)) {
@@ -47,6 +84,18 @@ public class NodeManager {
             return null;
         } catch (Exception e) {
             throw new ProcessException(e.getMessage(), e);
+        }
+    }
+
+    @Data
+    public static class NodeHeart {
+
+        private InetSocketAddress address;
+        private Long lastHeart;
+
+        public NodeHeart(InetSocketAddress address, Long lastHeart) {
+            this.address = address;
+            this.lastHeart = lastHeart;
         }
     }
 }
